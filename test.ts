87938346ef7af0a90321acec017e662194a206a4
meparthka <parthka.2005@gmail.com>
2022-06-09 15:06:00 +0530

    add

--- test.ts
+++ test.ts
@@ -1,4 +1,9 @@
-let tags1 = " "
-let tags2 = "sdfsdf,sdf ,sdf,sdf, sdf, sdf, sdf, dsff , sdf, f, df"
+import puppeteer from "https://deno.land/x/deno_puppeteer/mod.ts";
 
+const browser = await puppeteer.launch();
+const page = await browser.newPage();
+//await page.goto("https://alexandrempsantos.com/deno/puppeteer-with-deno/");
+await page.setContent("<h1>Hello, Parthka!");
+await page.pdf({ path: "example1.pdf" });
 
+await browser.close();
