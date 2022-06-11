e514a31c15e0e5c654a81210b9a3a3b76c15e396
meparthka <parthka.2005@gmail.com>
2022-06-11 12:03:06 +0530

    add mod

--- middleware/mod.ts
+++ middleware/mod.ts
@@ -2,7 +2,8 @@ import { db, ObjectId } from "./db.ts";
 import jsonCheck from "./rfieldjson.ts";
 import uuid from "./token.ts";
 import Tags from "./tags.ts";
+import htmlTemplate from "./template.ts"
 
 export type { ObjectId };
 
-export { db, jsonCheck, uuid, Tags };
+export { db, jsonCheck, uuid, Tags, htmlTemplate };
